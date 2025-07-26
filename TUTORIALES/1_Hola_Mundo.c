15ba5318d5a71f357751019574e63f0ab89840fa
Sam_Dev <axmillones@gmail.com>
2025-07-26 01:11:03 +0000

    Create 1_Hola_Mundo.c
    Toda información dentro del mismo archivo.

---
+++ TUTORIALES/1_Hola_Mundo.c
@@ -0,0 +1,41 @@
+/*Este será el primer tutorial de C donde explicaré algunas pequeñas cosas 
+También quiero decir que intentaré subir más de forma seguida pero esto lo hago para mi aprendisaje
+y de paso ayudar a todo el quiera iniciar ya que la mayoria por no decir que todo lo que abarca en el internet
+para aprender este lenguaje se encuentra en inglés
+
+Otra cosa:
+no busco copiar el contenido y apropiarme de él por esto aviso que todo lo que mostraré 
+se encentra en el libro "El Lenguaje de Programación C 2da Edición"
+solo que aqui todo esta más resumido basandome en la forma en que yo lo entendí
+
+así que por favor si encuentran algún error no duden en decirme 
+ 													GRACIAS.*/
+
+/* esto es un comentario de varias lineas que convierte todo lo que esté dentro 
+de los signos del incio y final */
+
+// se utiliza "//" para poner un comentario en la linea desde nonde inicie el misimo
+
+// #include como ya lo dice hace la función de añadir lirerias 
+// y en este caso la libreria seria stdio.h que significa entrada y salida estaandar
+
+#include <stdio.h>
+
+// omitiré el int y el void por ahora pero hay que saber que la funcion main() hace que el programa inicie
+// por lo que tine que estar en alguna parte del código
+
+int main(void)
+{
+ // aqui vemos la funcion printf que hace que en la pantalla se escriba la que se encuentra dentro de ella
+	
+	printf("Hola Mundo - Adios Mundo.\n");// el ";" se utiliza para finalizar cada funcion es por asi decir
+	// \n indica que se mostrará esto en una nueva línea   //el punto final de cada oración
+	
+	return 0; /*acá tenemos return que al finalizar le dice al programa que devuelva algun tipo de
+	información en esto caso el 0 que significa que no devuelva nada*/ 
+
+} /* y estos signos que veriamos luego del main y al final son los que encierran las funciones que se 
+inician dentro del main (se puede encontrar en muchas otras funciones) */ 
+
+	//Cualquier pregunta o alguien que quiera contactarme ya se por dudas o para colaborar 
+	//me pueden encontrar en face como Andry Samuel Pérez Batista luego añadiré mi insta.
